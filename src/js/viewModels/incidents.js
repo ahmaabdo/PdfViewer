f16817cce50dd849a99560c09142d506c5c7c542
AHMAD\ahmad <a7maabdo@gmail.com>
2018-05-07 14:29:02 +0200

    Add touch-id

--- src/js/viewModels/incidents.js
+++ src/js/viewModels/incidents.js
@@ -6,16 +6,32 @@
 /*
  * Your incidents ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils'],
- function(oj, ko, $, app, moduleUtils) {
-  
+define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-element-utils', 'ojs/ojbutton'],
+  function (oj, ko, $, app, moduleUtils) {
+
     function IncidentsViewModel() {
       var self = this;
 
+      //Touch-id Lib: https://github.com/EddyVerbruggen/cordova-plugin-touch-id
+      self.fingerPrintBtn = function (event) {
+
+        window.plugins.touchid.isAvailable(
+          function (type) { alert(type) }, // type returned to success callback: 'face' on iPhone X, 'touch' on other devices
+          function (msg) { alert('not available, message: ' + msg) } // error handler: no TouchID available
+        );
+
+        window.plugins.touchid.verifyFingerprint(
+          'Scan your fingerprint please', // this will be shown in the native scanner popup
+          function (msg) { alert('ok: ' + msg) }, // success handler: fingerprint accepted
+          function (msg) { alert('not ok: ' + JSON.stringify(msg)) } // error handler with errorcode and localised reason
+        );
+
+      }
+
       // Header Config
-      self.headerConfig = ko.observable({'view':[], 'viewModel':null});
-      moduleUtils.createView({'viewPath':'views/header.html'}).then(function(view) {
-        self.headerConfig({'view':view, 'viewModel':new app.getHeaderModel()})
+      self.headerConfig = ko.observable({ 'view': [], 'viewModel': null });
+      moduleUtils.createView({ 'viewPath': 'views/header.html' }).then(function (view) {
+        self.headerConfig({ 'view': view, 'viewModel': new app.getHeaderModel() })
       })
 
       // Below are a set of the ViewModel methods invoked by the oj-module component.
@@ -29,14 +45,14 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
        * and inserted into the DOM and after the View is reconnected 
        * after being disconnected.
        */
-      self.connected = function() {
+      self.connected = function () {
         // Implement if needed
       };
 
       /**
        * Optional ViewModel method invoked after the View is disconnected from the DOM.
        */
-      self.disconnected = function() {
+      self.disconnected = function () {
         // Implement if needed
       };
 
@@ -44,7 +60,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'appController', 'ojs/ojmodule-eleme
        * Optional ViewModel method invoked after transition to the new View is complete.
        * That includes any possible animation between the old and the new View.
        */
-      self.transitionCompleted = function() {
+      self.transitionCompleted = function () {
         // Implement if needed
       };
     }
